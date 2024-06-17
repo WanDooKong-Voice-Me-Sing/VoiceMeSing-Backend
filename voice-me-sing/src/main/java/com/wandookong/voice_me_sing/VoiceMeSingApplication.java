15e721e041cc08221b89c352b356e5728d14c2ca
Kyungbin Ryu <dfghj1009@naver.com>
2024-06-17 22:24:50 +0900

    initial settings

---
+++ voice-me-sing/src/main/java/com/wandookong/voice_me_sing/VoiceMeSingApplication.java
@@ -0,0 +1,13 @@
+package com.wandookong.voice_me_sing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceMeSingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceMeSingApplication.class, args);
+	}
+
+}
---
+++ voice-me-sing/src/test/java/com/wandookong/voice_me_sing/VoiceMeSingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandookong.voice_me_sing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceMeSingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
