d716398d93841c250116b8afe77ef7fe00d5d2fa
Kyungbin Ryu <dfghj1009@naver.com>
2024-11-05 01:40:37 +0900

    Chore: CommunityController Swagger 문서화

--- voice-me-sing/src/main/java/com/wandookong/voice_me_sing/controller/CommunityController.java
+++ voice-me-sing/src/main/java/com/wandookong/voice_me_sing/controller/CommunityController.java
@@ -3,6 +3,13 @@ package com.wandookong.voice_me_sing.controller;
 import com.wandookong.voice_me_sing.dto.CoverSongDTO;
 import com.wandookong.voice_me_sing.dto.ResponseDTO;
 import com.wandookong.voice_me_sing.service.SongService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,10 +19,26 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Tag(name = "Community Forum API", description = "Handles operations related to cover songs\n커뮤니티와 관련된 작업을 처리")
 public class CommunityController {
 
     private final SongService songService;
 
+    @Operation(
+            summary = "전체 공개 커버곡 조회",
+            description = "모든 공개된 커버곡 리스트를 조회"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "전체 커버곡 조회 성공\nSuccessfully retrieved all cover songs",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDTO.class),
+                            examples = @ExampleObject(value = "{\"status\":\"success\",\"message\":\"get all cover songs successfully\",\"data\":[{\"coverSongId\":1,\"resultSongName\":\"Song A\",\"coverSongFile\":\"fileA.mp3\",\"isPublic\":true},{\"coverSongId\":2,\"resultSongName\":\"Song B\",\"coverSongFile\":\"fileB.mp3\",\"isPublic\":true}]}")
+                    )
+            )
+    })
     @GetMapping("/community")
     public ResponseEntity<?> getAllPublicCoverSongs() {
 
